31e0f65beb78fac774f50eb9c7377eedf4b4f68d
0129823198 <85694424+0129823198@users.noreply.github.com>
2021-06-10 12:48:54 -0300

    Add files via upload

---
+++ scrotum.bat
@@ -0,0 +1,50 @@
+reg delete "HKEY_CURRENT_USER\Control Panel\Mouse" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks\Guests\Android\sensibility\0" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks\Guests\Android\sensibility\1" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks\Guests\Android\sensibility\2" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks\Guests\Android\sensibility\3" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks\Guests\Android\sensibility\4" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks\Guests\Android\sensibility\5" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\SmartGaGa\ProjectTitan\sensibility\0" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\SmartGaGa\ProjectTitan\sensibility\1" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\SmartGaGa\ProjectTitan\sensibility\2" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\SmartGaGa\ProjectTitan\sensibility\3" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\SmartGaGa\ProjectTitan\sensibility\4" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\SmartGaGa\ProjectTitan\sensibility\5" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Memu\sensibility\0" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Memu\sensibility\1" /f  
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Memu\sensibility\2" /f 
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Memu\sensibility\3" /f 
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Memu\sensibility\4" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Memu\sensibility\5" /f  
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_msi2\Guests\Android\sensibility\0" /f 
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_msi2\Guests\Android\sensibility\1" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_msi2\Guests\Android\sensibility\2" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_msi2\Guests\Android\sensibility\3" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_msi2\Guests\Android\sensibility\4" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_msi2\Guests\Android\sensibility\5" /f
+
